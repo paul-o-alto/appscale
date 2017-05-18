889ce427230981ed4310a3fe23cec1be72f4082d
Paul Galloway <galloway.paul.f@gmail.com>
2017-05-18 09:42:39 -0700

    removed dev tag

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ APPSCALE_BRANCH="master"
 APPSCALE_TOOLS_BRANCH="master"
 FORCE_UPGRADE="N"
 UNIT_TEST="n"
-GIT_TAG="dev"
+GIT_TAG=""
 
 usage() {
     echo "Usage: ${0} [--repo <repo>][--tools-repo <repo>][-t]"
